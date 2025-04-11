d12aacc40f0664abcc56c8c0d6d2f9459dd7259d
drahmetacik <a.acik@icloud.com>
2025-04-11 11:58:58 +0100

    Load MongoDB connection settings from application.properties for dynamic configuration

--- src/main/java/org/mongodbproject/config/MongoDBConnection.java
+++ src/main/java/org/mongodbproject/config/MongoDBConnection.java
@@ -4,9 +4,25 @@ import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoDatabase;
 
+import java.io.IOException;
+import java.util.Properties;
+
 public class MongoDBConnection {
-    private static final String CONNECTION_STRING = "mongodb://localhost:27017";
-    private static final String DATABASE_NAME = "practiceDB";
+    private static final String CONNECTION_STRING;
+    private static final String DATABASE_NAME;
+
+    static {
+        Properties properties = new Properties();
+        try {
+            properties.load(MongoDBConnection.class.getClassLoader().getResourceAsStream("application.properties"));
+            String host = properties.getProperty("spring.data.mongodb.host", "localhost");
+            String port = properties.getProperty("spring.data.mongodb.port", "27017");
+            CONNECTION_STRING = "mongodb://" + host + ":" + port;
+            DATABASE_NAME = properties.getProperty("spring.data.mongodb.database", "practiceDB");
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to load MongoDB configuration from application.properties", e);
+        }
+    }
 
     public static MongoDatabase getDatabase() {
         MongoClient mongoClient = MongoClients.create(CONNECTION_STRING);
