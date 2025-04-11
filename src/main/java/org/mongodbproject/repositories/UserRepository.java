907cd54f1a59499219ccf41ea3bb55bced41864d
drahmetacik <a.acik@icloud.com>
2025-04-11 11:33:48 +0100

    Add logging to UserRepository for database operations

--- src/main/java/org/mongodbproject/repositories/UserRepository.java
+++ src/main/java/org/mongodbproject/repositories/UserRepository.java
@@ -2,30 +2,38 @@ package org.mongodbproject.repositories;
 
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
 import org.mongodbproject.config.MongoDBConnection;
 import org.mongodbproject.models.User;
-import org.bson.Document;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class UserRepository {
+    private static final Logger logger = LoggerFactory.getLogger(UserRepository.class);
     private final MongoCollection<Document> collection;
 
     public UserRepository() {
         MongoDatabase database = MongoDBConnection.getDatabase();
         this.collection = database.getCollection("users");
+        logger.info("Connected to MongoDB collection: users");
     }
 
     public void insertUser(User user) {
+        logger.info("Inserting user: {}", user);
         collection.insertOne(user.toDocument());
+        logger.info("User inserted successfully: {}", user);
     }
 
     public List<User> getAllUsers() {
+        logger.info("Retrieving all users from the database");
         List<User> users = new ArrayList<>();
         for (Document doc : collection.find()) {
             users.add(User.fromDocument(doc));
         }
+        logger.info("Retrieved {} users from the database", users.size());
         return users;
     }
 }
