e253637b669b05681d6c2494525cf7743aff9fec
drahmetacik <a.acik@icloud.com>
2025-04-11 11:41:16 +0100

    Add Address model and update User model to include address and hobbies

--- src/main/java/org/mongodbproject/models/User.java
+++ src/main/java/org/mongodbproject/models/User.java
@@ -2,39 +2,26 @@ package org.mongodbproject.models;
 
 import org.bson.Document;
 
+import java.util.List;
+
 public class User {
     private String id;
     private String name;
     private String email;
+    private Address address; // Nested object
+    private List<String> hobbies; // Array of strings
 
     // Getters and setters
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
     public Document toDocument() {
-        return new Document("name", name).append("email", email);
+        Document addressDoc = new Document("street", address.getStreet())
+                .append("city", address.getCity())
+                .append("zipCode", address.getZipCode());
+
+        return new Document("name", name)
+                .append("email", email)
+                .append("address", addressDoc)
+                .append("hobbies", hobbies);
     }
 
     public static User fromDocument(Document document) {
@@ -42,6 +29,17 @@ public class User {
         user.setId(document.getObjectId("_id").toString());
         user.setName(document.getString("name"));
         user.setEmail(document.getString("email"));
+
+        Document addressDoc = document.get("address", Document.class);
+        if (addressDoc != null) {
+            Address address = new Address();
+            address.setStreet(addressDoc.getString("street"));
+            address.setCity(addressDoc.getString("city"));
+            address.setZipCode(addressDoc.getString("zipCode"));
+            user.setAddress(address);
+        }
+
+        user.setHobbies(document.getList("hobbies", String.class));
         return user;
     }
 }
