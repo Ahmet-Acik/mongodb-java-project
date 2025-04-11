9770ed1469aa34a5311c167a6db2816847530162
drahmetacik <a.acik@icloud.com>
2025-04-11 11:42:48 +0100

    Add getters and setters for User fields including id, name, email, address, and hobbies

--- src/main/java/org/mongodbproject/models/User.java
+++ src/main/java/org/mongodbproject/models/User.java
@@ -13,6 +13,46 @@ public class User {
 
     // Getters and setters
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public List<String> getHobbies() {
+        return hobbies;
+    }
+
+    public void setHobbies(List<String> hobbies) {
+        this.hobbies = hobbies;
+    }
+
     public Document toDocument() {
         Document addressDoc = new Document("street", address.getStreet())
                 .append("city", address.getCity())
