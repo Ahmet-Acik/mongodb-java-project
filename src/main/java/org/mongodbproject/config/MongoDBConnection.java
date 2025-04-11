642d45b6fec0cdf29fb06b922e893f3a9dae80ce
drahmetacik <a.acik@icloud.com>
2025-04-11 11:30:08 +0100

    Implement user management functionality with MongoDB integration

---
+++ src/main/java/org/mongodbproject/config/MongoDBConnection.java
@@ -0,0 +1,15 @@
+package org.mongodbproject.config;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+
+public class MongoDBConnection {
+    private static final String CONNECTION_STRING = "mongodb://localhost:27017";
+    private static final String DATABASE_NAME = "practiceDB";
+
+    public static MongoDatabase getDatabase() {
+        MongoClient mongoClient = MongoClients.create(CONNECTION_STRING);
+        return mongoClient.getDatabase(DATABASE_NAME);
+    }
+}
